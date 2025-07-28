4e71951937808e1051353015c4451e074a4283ac
AmKPrjectRS <alyanmo96@gmail.com>
2025-07-28 12:08:37 +0300

    remove_exclamation_marks

---
+++ Level - 8/remove_exclamation_marks.py
@@ -0,0 +1,9 @@
+#    Removes all exclamation marks (!) from the given string.
+
+
+def remove_exclamation_marks(s):
+
+
+    return s.replace('!','')
+# Example usage
+print(remove_exclamation_marks("Jer!sa!!cvg23!!!"))  # Output: 'Jersacvg23'
